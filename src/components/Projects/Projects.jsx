22e89f6206d24069292858f79089cf8a8b4dd129
AnikHaque <ekramul15-811@diu.edu.bd>
2023-11-07 19:14:01 +0600

    updated audience section

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Services from '../Services/Services'
+import { ProjectsReview } from '../Services/ProjectsReview'
+import { Audience } from './Audience'
 
 export const Projects = () => {
   return (
     <div>
         <Services></Services>
+        <br></br>
+        <br></br>
+        <br></br>
+        <Audience></Audience>
+          <br></br>
+          <br></br>
+          <br></br>
+        <ProjectsReview></ProjectsReview>
     </div>
   )
 }
